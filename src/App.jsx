cfd73f03d85fe1109589867fa150acc445d8e217
Geiselle Holt <geiselleholt@hotmail.com>
2025-05-08 13:55:52 -0600

    added MovieDisplay and form logic, fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import MovieDisplay from "./components/MovieDisplay";
-import Form from "./components/Form";
+import MovieDisplay from "./components/MovieDisplay.jsx";
+import Form from "./components/Form.jsx";
 
 function App() {
+  // Constant with your API Key
+  const apiKey = "98e3fb1f";
 
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async(searchTerm) => {
+    // Make fetch request and store the response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a JavaScript object
+    const data = await response.json();
+    // Set the Movie state to the received data
+    setMovie(data);
+  };
 
   return (
     <div className="App">
-      <Form />
-      <MovieDisplay />
+      <Form moviesearch={getMovie} />
+       <MovieDisplay movie={movie} />
     </div>
   );
 }
--- src/components/Form.mjs
+++
@@ -1,4 +0,0 @@
-export default function Form(props){
-  // The component must return some JSX
-  return <h1>The Form Component</h1>;
-};
