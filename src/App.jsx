0f5b3adad51366205c1417b68880fd413ae71a9d
Geiselle Holt <geiselleholt@hotmail.com>
2025-05-08 13:57:58 -0600

    lab done and tested

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,31 @@ import MovieDisplay from "./components/MovieDisplay.jsx";
 import Form from "./components/Form.jsx";
 
 function App() {
-  // Constant with your API Key
   const apiKey = "98e3fb1f";
 
-  // State to hold movie data
   const [movie, setMovie] = useState(null);
 
-  // Function to get movies
-  const getMovie = async(searchTerm) => {
-    // Make fetch request and store the response
-    const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
-    );
-    // Parse JSON response into a JavaScript object
-    const data = await response.json();
-    // Set the Movie state to the received data
-    setMovie(data);
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (e) {
+      console.error(e);
+    }
   };
+
+  // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
 
   return (
     <div className="App">
       <Form moviesearch={getMovie} />
-       <MovieDisplay movie={movie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
